0de18852e86b0c5dcfacae495ddb581a469a9f31
obaraelijah <elijahobara357@gmail.com>
2024-05-20 14:02:27 +0300

    feat: Server setup

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-mod query_root;
-mod mutation_root;
-mod domain;
-mod command;
-mod server;
-mod query;
 mod api;
+mod command;
+mod domain;
+mod mutation_root;
+mod query;
+mod query_root;
+mod server;
 
-use async_graphql::{http::GraphiQLSource, Schema, EmptySubscription};
+use async_graphql::{http::GraphiQLSource, EmptySubscription, Schema};
 use async_graphql_axum::GraphQL;
 use axum::{
-    response::{Html, IntoResponse}, routing::get, Router
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
 };
 use std::error::Error;
 
 use crate::{mutation_root::Mutation, query_root::QueryRoot};
 
-async fn graphql_handler() -> impl IntoResponse {
+async fn graphiql() -> impl IntoResponse {
     Html(GraphiQLSource::build().finish())
 }
 
@@ -23,16 +25,17 @@ async fn graphql_handler() -> impl IntoResponse {
 async fn main() -> Result<(), Box<dyn Error>> {
     let schema = Schema::build(QueryRoot, Mutation, EmptySubscription).finish();
 
+    let shared_state = server::new()?;
     let app = Router::new()
-        .route("/", get(graphql_handler).post_service(GraphQL::new(schema)))
-        .route("/version", get(version));
-
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .route("/", get(graphiql).post_service(GraphQL::new(schema)))
+        .route("/version", get(version))
+        .with_state(shared_state);
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
     println!("Listening on: {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
-    Ok(())    
+    Ok(())
 }
 
 async fn version() -> impl IntoResponse {
--- src/server.rs
+++ src/server.rs
@@ -1,12 +1,33 @@
 use std::sync::Arc;
 
 use crate::{
+    api::app_state::AppState,
     command::{
         command_handler::CommandHandler,
         interface::circle_repository_interface::{CircleRepositoryInterface, HasCircleRepository},
-    }
+    },
+    query::query_handler::QueryHandler,
 };
 
-pub fn new() -> anyhow::Result<()> {
-    Ok(())
+pub fn new() -> anyhow::Result<AppState> {
+    struct CommandHandlerStruct {
+        circle_repository: Arc<dyn CircleRepositoryInterface + Send + Sync>,
+    }
+    impl HasCircleRepository for CommandHandlerStruct {
+        fn circle_repository(&self) -> Arc<dyn CircleRepositoryInterface + Send + Sync> {
+            self.circle_repository.clone()
+        }
+    }
+    impl CommandHandler for CommandHandlerStruct {}
+    let command_handler = CommandHandlerStruct {
+        circle_repository: todo!()
+    };
+
+    struct QueryHandlerStruct {}
+    impl QueryHandler for QueryHandlerStruct {}
+    let query_handler = QueryHandlerStruct {};
+    Ok(AppState::new(
+        Arc::new(command_handler),
+        Arc::new(query_handler),
+    ))
 }
